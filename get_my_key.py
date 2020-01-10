60681fa3c7047aeb1959be5bb1c21f721ed2fa47
Padraic Doran <padraic.doran@gmail.com>
2020-01-10 09:59:51 -0600

    Fixed a function

---
+++ get_my_key.py
@@ -0,0 +1,9 @@
+def client_id():
+    Client_ID = '8686c761d3182d6d8499bf7ef711556921f22a2b'
+    return Client_ID
+
+
+def api_key():
+    api_key = "RtDiIsUaEE5iZJGscWuiVJ3x-Gi6aHkDIdfv44NZphf7kJa3R4G68dCYmO0nkmMi6OEqpmPbqPLLz7MLu20Xw39qTOtCDloyDFmD5XQuVaA42KVKsTRiYsUv-qMXXnYx"
+    return api_key
+    
